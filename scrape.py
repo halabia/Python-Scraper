4fc96054a7d87f608294372297f866d2bfcaed69
Alanjim Halabia <alanjim.halabia@hotmail.com>
2022-08-16 02:51:10 -0400

    soup find element id

--- scrape.py
+++ scrape.py
@@ -5,3 +5,7 @@ URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+print(results.prettify())
