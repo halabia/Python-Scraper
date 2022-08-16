f4bc2d6bafeb5c68ea6ebc365cf8a1433dcae799
Alanjim Halabia <alanjim.halabia@hotmail.com>
2022-08-16 02:46:21 -0400

    beautiful soup test

--- scrape.py
+++ scrape.py
@@ -1,8 +1,7 @@
-#initial test
-
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
